75a6e13d42dbd86deae5ff487851c3b711d55b27
Laurence Mantiza <laurencemantiza2@gmail.com>
2026-09-10 10:24:59 +0400

    Restore admin mode routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import App from './App.jsx'
-import Admin from './Admin.jsx'
 
 const isAdminPage =
   window.location.pathname.replace(/\/$/, '') === '/admin'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {isAdminPage ? <Admin /> : <App />}
+    <App adminMode={isAdminPage} />
   </StrictMode>,
 )
