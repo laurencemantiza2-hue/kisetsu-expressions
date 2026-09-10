38f141cc2b46991c2e48399bb68cf17b86637581
Laurence Mantiza <laurencemantiza2@gmail.com>
2026-09-10 10:21:35 +0400

    Separate public website from admin editor

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-const isAdminPage = window.location.pathname.replace(/\/$/, '') === '/admin'
+import './index.css'
+
+import App from './App.jsx'
+import Admin from './Admin.jsx'
+
+const isAdminPage =
+  window.location.pathname.replace(/\/$/, '') === '/admin'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App adminMode={isAdminPage} />
+    {isAdminPage ? <Admin /> : <App />}
   </StrictMode>,
 )
